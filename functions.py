635ea63bef9fcb64cf0ebb988a9313d48c56d8f5
DAMIEN SINGH <dsingh005@citymail.cuny.edu>
2020-06-18 12:33:27 -0400

    finised base game

--- functions.py
+++ functions.py
@@ -1,20 +1,23 @@
 
 import pygame
 import sys
+import random
+
 # Colors
 #       R  G  B
 RED = (255, 0, 0,)
 GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
+BLUE = (0, 0, 255)
 BLACK = (0, 0, 0,)
 WHITE = (255, 255, 255)
+GREY = (100, 100, 100)
 
 
 class cell(object):
     rows = 20
     w = 500
 
-    def __init__(self, start, dirX=0, dirY=1, color=RED):
+    def __init__(self, start, dirX=0, dirY=1, color=BLUE):
         self.pos = start
         self.dirX = 1
         self.dirY = 0
@@ -30,7 +33,7 @@ class cell(object):
         i = self.pos[0]  # x direction ROW
         j = self.pos[1]  # y direction COLUMN
         # draw cell
-        #                      We draw cube but we want it be within the grid
+        # ---------------------- We draw cube but we want it be within the grid
         pygame.draw.rect(surface, self.color, (i*dis+1, j*dis+1, dis-2, dis-2))
         if eyes:
             center = dis//2
@@ -58,7 +61,7 @@ class snake(object):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                sys.exit
+                sys.exit()
             # Gives list of keys pressed
             keys = pygame.key.get_pressed()
 
@@ -91,26 +94,41 @@ class snake(object):
                 if i == len(self.body)-1:
                     self.turns.pop(p)
         # Lets check if we have reached the edge of the screen
-        else:
-            # if we move all the way to the left, turn but keep the y direction
-            if c.dirX == -1 and c.pos[0] <= 0:
-                c.pos = (c.rows-1, c.pos[1])
-            elif c.dirX == 1 and c.pos[0] >= c.rows-1:
-                c.pos = (0, c.pos[1])
-            # We go down, move back to the top, change y only
-            elif c.dirY == 1 and c.pos[1] >= c.rows-1:
-                c.pos = (c.pos[0], 0)
-            # We go up, change to down
-            elif c.dirY == -1 and c.pos[1] <= 0:
-                c.pos = (c.pos[0], c.rows-1)
             else:
-                c.move(c.dirX, c.dirY)
+                if c.dirX == -1 and c.pos[0] <= 0:
+                    c.pos = (c.rows-1, c.pos[1])
+                elif c.dirX == 1 and c.pos[0] >= c.rows-1:
+                    c.pos = (0, c.pos[1])
+                elif c.dirY == 1 and c.pos[1] >= c.rows-1:
+                    c.pos = (c.pos[0], 0)
+                elif c.dirY == -1 and c.pos[1] <= 0:
+                    c.pos = (c.pos[0], c.rows-1)
+                else:
+                    c.move(c.dirX, c.dirY)
 
     def reset(self, pos):
-        pass
+        self.head = cell(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
 
     def addCell(self):
-        pass
+        tail = self.body[-1]
+        dx, dy = tail.dirX, tail.dirY
+
+        if dx == 1 and dy == 0:
+            self.body.append(cell((tail.pos[0]-1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cell((tail.pos[0]+1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cell((tail.pos[0], tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cell((tail.pos[0], tail.pos[1]+1)))
+
+        self.body[-1].dirX = dx
+        self.body[-1].dirY = dy
 
     def draw(self, surface):
         for i, c in enumerate(self.body):
@@ -118,3 +136,16 @@ class snake(object):
                 c.draw(surface, True)
             else:
                 c.draw(surface)
+
+
+def randomFood(rows, item):
+    positions = item.body
+
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+    return (x, y)
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
 import functions
-import random
-import math
 import tkinter as tk
 from tkinter import messagebox
 from functions import snake
-pygame.init()
+from functions import cell, randomFood
+
+
+def message_box(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
 
 
 def drawGrid(w, rows, surface):
@@ -16,33 +25,45 @@ def drawGrid(w, rows, surface):
         x = x + gapSize
         y = y + gapSize
         # Draw the lines                    Start Position    End Position
-        pygame.draw.line(surface, functions.WHITE, (x, 0), (x, w))
-        pygame.draw.line(surface, functions.WHITE, (0, y), (w, y))
+        pygame.draw.line(surface, functions.GREY, (x, 0), (x, w))
+        pygame.draw.line(surface, functions.GREY, (0, y), (w, y))
 
 
 def redrawWindow(surface):
-    global rows, width, s
+    global rows, width, s, apple
     surface.fill(functions.BLACK)
     s.draw(surface)
+    apple.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
 
 
 def main():
-    global width, rows, s
+    global width, rows, s, apple
     width = 500
     rows = 20
     displayWindow = pygame.display.set_mode((width, width))
     running = True
     clock = pygame.time.Clock()
-    s = snake(functions.RED, (10, 10))
+    s = snake(functions.BLUE, (10, 10))
+    apple = cell(randomFood(rows, s), color=(255, 0, 0))
     # Main Loop
     while running:
-        # Speed
-        pygame.time.delay(50)  # Lower = faster
-        clock.tick(10)  # Lower = slower
+        pygame.time.delay(50)
+        clock.tick(10)
         s.move()
-        redrawWindow(displayWindow)
+        if s.body[0].pos == apple.pos:
+            s.addCell()
+            apple = cell(randomFood(rows, s), color=(255, 0, 0))
+
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z: z.pos, s.body[x+1:])):
+                print('Score: ', len(s.body))
+                message_box('You Lost!', 'Play again...')
+                s.reset((10, 10))
+                break
+
+            redrawWindow(displayWindow)
 
 
 main()
